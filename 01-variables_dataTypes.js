e5959e38154e7c90f86e5f8d7bdb561c9d79a64b
mahveen-fatima <mahveenfatima2224@gmail.com>
2024-07-13 18:31:14 +0530

    first commit

---
+++ 01-variables_dataTypes.js
@@ -0,0 +1,68 @@
+// Day 1: Variables and Data Types
+//1. Variable Declaration
+var num = 2
+console.log(num);
+
+let text = "Hello world"
+console.log(text);
+
+//2. Constant Declaration
+const isActive = true
+console.log(isActive);
+
+// //3. Data Types
+// const myNum = 4
+// console.log(myNum);
+// console.log(typeof myNum);
+
+// const myText = "Hello world"
+// console.log(myText);
+// console.log(typeof myText);
+
+// const isPaid = false
+// console.log(isPaid);
+// console.log(typeof isPaid);
+
+// const myObj = {
+//     username : "mahveen",
+//     password: 12345
+// }
+// console.log(myObj);
+// console.log(typeof myObj);
+
+// const myArr = ["java", "javascript", "python"]
+// console.log(myArr);
+// console.log(typeof myArr);
+
+
+// 3. All Data types with log and typeof
+console.log("Data types with value and type of");
+let myNum = 10
+let myStr = "Hello world"
+let isLoggedIn = true
+let myObj = {
+    username: "Hello world",
+    password: 123
+}
+let myArr = [1, 2, 3, 4, 5]
+
+console.log(myNum, typeof myNum);
+console.log(myStr, typeof myStr);
+console.log(isLoggedIn, typeof isLoggedIn);
+console.log(myObj, typeof myObj);
+console.log(myArr, typeof myArr);
+
+
+// 4.Reassigning Variables
+
+let value = "initial value"
+console.log(value);
+value = "reassign value"
+console.log(value);
+
+// 5. Understanding const
+const newValue = "first value"
+console.log(newValue);
+newValue = "second value"
+console.log(newValue); //cannot reassign in const
+
